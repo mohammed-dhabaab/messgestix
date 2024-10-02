438bb734b9d80877e39827eac7cc0cdbef09eaf1
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-10-02 07:21:15 +0300

    Completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState, } from 'react'
 import Phone from './Phone'
 import styles from '../styles'
 function Home() {
@@ -12,12 +12,13 @@ function Home() {
     const [displayPhones, setDisplayPhones] = useState(false)
 
     const start = () => {
-        setDisplayPhones(true)
-        // userName1 !== "" && userName2 !== "" ? setDisplayPhones(true) : ""
+        if (userName1 && userName2 && userName1 !== userName2 && userName1.length < 10 && userName2.length < 10) {
+            setDisplayPhones(true)
+        }
     }
     return (
         <main className={`${styles.outerWrapper}`}>
-            <div className={`${styles.wrapper} flex flex-col flex-wrap justify-center items-center sm:flex-row gap-10`}>
+            <div className={`${styles.wrapper} relative flex flex-col flex-wrap justify-center items-center sm:flex-row gap-10`}>
 
                 {
                     displayPhones ?
@@ -32,8 +33,25 @@ function Home() {
                                 <div className="display">
                                     <div className="artboard artboard-demo phone-1 flex flex-col gap-6 p-4">
                                         <h1 className='text-xl md:text-2xl font-semibold mb-4'>Welcome</h1>
-                                        <input type="text" onChange={(e) => setUserName1(e.target.value)} placeholder="Type first user name" className="input input-bordered w-full max-w-xs" />
-                                        <input type="text" onChange={(e) => setUserName2(e.target.value)} placeholder="Type second user name" className="input input-bordered w-full max-w-xs" />
+                                        <div>
+                                            <input type="text" onChange={(e) => setUserName1(e.target.value)} placeholder="Type first user name" className="input input-bordered w-full max-w-xs" />
+                                            <input type="text" onChange={(e) => setUserName2(e.target.value)} placeholder="Type second user name" className="mt-5 mb-3 input input-bordered w-full max-w-xs" />
+                                            <div className='flex gap-2 items-center text-xs text-gray-500'>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    className="h-4 w-4 shrink-0 stroke-current">
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                                </svg>
+                                                <span>Please, no empty names and not equal, and &lt; 10 letters</span>
+                                            </div>
+
+                                        </div>
                                         <button onClick={start} className='btn btn-primary'>Start</button>
                                     </div>
                                 </div>
--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useRef } from 'react';
 import Message from './Message';
 import { BsFillSendFill } from "react-icons/bs";
+import { PiSelectionBackgroundBold } from "react-icons/pi";
 
 function Phone({ user, messages, setMessages, background, receiver, receiverPhoto }) {
     const [messageContent, setMessageContent] = useState("");
     // const messagesEndRef = useRef(null); // Create a ref for scrolling  
+    const [backgroundSrc, setBackgroundSrc] = useState(background)
+    const [receiverPhotoSrc, setReceiverPhotoSrc] = useState(receiverPhoto)
+    const [backgroundLink, setBackgroundLink] = useState("")
+    const [receiverPhotoLink, setReceiverPhotoLink] = useState("")
+    const [changeImages, setChangeIMages] = useState(false)
 
     const sendMessage = () => {
         if (messageContent) {
@@ -55,20 +61,59 @@ function Phone({ user, messages, setMessages, background, receiver, receiverPhot
         }
     };
 
+
+    const setImages = () => {
+        if (backgroundLink !== "" && receiverPhotoLink !== "") {
+            setBackgroundSrc(backgroundLink)
+            setReceiverPhotoSrc(receiverPhotoLink)
+            setChangeIMages(false)
+        }
+    }
+
     return (
         <div className='w-fit'>
             <h2 className='text-center'>{user}</h2>
             <div className="mockup-phone">
                 <div className="camera"></div>
                 <div className="display">
-                    <div className="relative flex-1 artboard artboard-demo phone-1 flex flex-col justify-end py-4 px-0 bg-center bg-no-repeat bg-cover"
-                        style={{ backgroundImage: `url('${background}')` }}
+                    <div className="relative flex-1 artboard artboard-demo phone-1 flex flex-col justify-end pb-4 px-0 bg-center bg-no-repeat bg-cover"
+                        style={{ backgroundImage: `url('${backgroundSrc}')` }}
                     >
-                        <div className='absolute z-10 top-0 bg-slate-800 w-full p-3 pt-6 flex items-center gap-2'>
-                            <div>
-                                <img className='w-[50px] h-[50px] rounded-full' src={receiverPhoto} alt="Photo" />
+
+                        <div className='bg-slate-800 w-full p-3 pt-6 flex items-center justify-between'>
+                            <div className='flex items-center gap-2'>
+                                <div>
+                                    <img className='w-[50px] h-[50px] rounded-full' src={receiverPhotoSrc} alt="Photo" />
+                                </div>
+                                <div className='flex flex-col'>
+                                    <p className='text-lg text-gray-300'>{receiver}</p>
+                                    <p className='text-xs text-success'>Online</p>
+                                </div>
                             </div>
-                            <p className='text-lg text-gray-300'>{receiver}</p>
+
+                            <div onClick={() => setChangeIMages(true)} title='Change Images' className='p-2 bg-slate-600 rounded-full text-gray-100 cursor-pointer'>
+                                <PiSelectionBackgroundBold />
+                            </div>
+                        </div>
+
+                        <div className={`${changeImages ? "flex" : "hidden"} bg-gray-800 p-2 w-3/4 rounded-md h-fit flex-col gap-2  absolute inset-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}>
+                            <input onChange={((e) => setReceiverPhotoLink(e.target.value))} type="text" placeholder="Photo Link" className="input input-bordered w-full max-w-xs placeholder:text-xs" />
+                            <input onChange={(e) => setBackgroundLink(e.target.value)} type="text" placeholder="Background Link" className="input input-bordered w-full max-w-xs placeholder:text-xs" />
+                            <div className='flex gap-2 items-center text-xs text-gray-500'>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    className="h-4 w-4 shrink-0 stroke-current">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
+                                <span>Please, not empty</span>
+                            </div>
+                            <button onClick={setImages} className='btn' >Submit</button>
                         </div>
 
                         <div className='w-full overflow-y-auto h-full'> {/* Add h-full or max-h to allow scrolling */}
